5704f6b3181c2e1f243b08f05e4cfaae49703f32
Neil Agrawal <neil.agrawal12@gmail.com>
2022-07-25 10:11:16 -0400

    Changed DOI to Link because its not a DOI

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ search_query = scholarly.search_pubs("what you want to search on google scholar"
 
 with open('filename.csv', 'w', encoding="utf8", newline='') as f:   
     thewriter = writer(f)
-    header = ["Title", "Authors", "Year", "Journal", "Abstract", "DOI"]
+    header = ["Title", "Authors", "Year", "Journal", "Abstract", "Link"]
     thewriter.writerow(header)
 
     for i in range(200): 
         publication = next(search_query)
         fullpublication = publication['bib']
         if ("pub_url" in publication):
-            fullpublication['DOI'] = publication["pub_url"]
+            fullpublication['Link'] = publication["pub_url"]
         else: 
-            fullpublication['DOI'] = "None"
+            fullpublication['Link'] = "None"
         thewriter.writerow(list(fullpublication.values()))
 
