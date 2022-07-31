0eb6404fb94c7b97c7590aebf69627fca4c65147
Neil Agrawal <neil.agrawal12@gmail.com>
2022-07-31 17:05:37 -0400

    fetch the title, full abstract, and doi from pubmed articles now.

--- pubmed.py
+++ pubmed.py
@@ -1,5 +1,6 @@
 from Bio import Entrez
 import sys
+import xml.etree.ElementTree as ET
 
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     enc = file.encoding
@@ -11,14 +12,38 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
 
 
 Entrez.email="lenter360@gmail.com"
-handle = Entrez.esearch(db="pubmed", term="lucid dreaming", retmax="4", retmode="xml",)
+handle = Entrez.esearch(db="pubmed", term="lucid dreaming", retmax="200", retmode="xml",)
 rec_list = Entrez.read(handle)
 id_list = rec_list['IdList']
 
 handle = Entrez.efetch(db="pubmed", id=id_list, retmode="xml") #rettype="abstract")
 records = handle.read().decode('utf-8')
-uprint(records)
+# uprint(records)
 
 with open ("information.xml", "w", encoding='utf-8') as xml_file: 
     xml_file.write(records)
 
+mytree = ET.parse('information.xml')
+root = mytree.getroot()
+# uprint(myroot[0][0][2][4][0].text)
+
+counter = 1
+for elm in root.findall("./PubmedArticle/MedlineCitation/Article"): 
+    uprint(f"Title {counter}: " + elm.find('ArticleTitle').text)
+    abstractWrapper = elm.find('Abstract')
+    if abstractWrapper is None: 
+        abstract = "No Abstract" 
+    else: 
+        abstract = abstractWrapper.find('AbstractText').text
+
+    if abstract is None: 
+        abstract = "No Abstract"
+        
+    uprint("Abstract: " + abstract)
+    doi = elm.find("ELocationID")
+    if doi is None: 
+        doi = "No DOI"
+    else: 
+        doi = doi.text
+    uprint("DOI: http://doi.org/" + doi + "\n")
+    counter = counter + 1
